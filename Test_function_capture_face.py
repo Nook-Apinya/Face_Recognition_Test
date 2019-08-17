008601d1aa796f50b9fa9ddebcf8c8fa23556c1f
Nook-Apinya <Nook1Apinya@gmail.com>
2019-08-17 15:31:30 +0700

    commit1

--- Test_function_capture_face.py
+++ Test_function_capture_face.py
@@ -7,7 +7,7 @@ image=cv2.VideoCapture(0)
 ret, img = image.read()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = cascade.detectMultiScale(gray, 1.3, 5)
+faces = cascade.detectMultiScale(gray, 1.4, 5)
 for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
