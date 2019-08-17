9155b01dcaebfe58a23df3d2a38526108fc65903
Nook-Apinya <Nook1Apinya@gmail.com>
2019-08-17 15:30:37 +0700

    commit

---
+++ Test_function_capture_face.py
@@ -0,0 +1,17 @@
+import cv2 
+import numpy as np   
+
+cascade=cv2.CascadeClassifier(r'C:\Users\Administrator\Documents\For Python\Face recognition Test\Face_Recognition_Test\haarcascade_frontalface_default.xml')
+image=cv2.VideoCapture(0)
+
+ret, img = image.read()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+cv2.imshow('Face detection',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
